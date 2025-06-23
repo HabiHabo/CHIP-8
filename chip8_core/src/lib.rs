ced39d2da669588019267fc60bded2d8cd149b17
Dijar-Düzgün Somuncuyan <kartoffelheld2@gmail.com>
2025-06-23 21:41:34 +0200

    Emulation methods, struct and half of opcodes

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::random;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -12,3 +14,258 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+const FONTSET_SIZE: usize = 80;
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+];
+
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+
+pub struct Emu {
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
+    i_reg: u16,
+    sp: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    dt: u8,
+    st: u8,
+}
+
+const START_ADDR: u16 = 0x200;
+
+impl Default for  Emu {
+    fn default() -> Self {
+        let mut new_emu = Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emu
+    }
+}
+
+impl Emu {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn reset(&mut self) {
+        *self = Self::default();
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize]
+    }
+
+    pub fn tick(&mut self) {
+        // Fetch
+        let op = self.fetch();
+        // Decode and Execute
+        self.execute(op);
+    }
+
+    fn execute(&mut self, op: u16) {
+        let digit1 = (op & 0xF000) >> 12;
+        let digit2 = (op & 0x0F00) >> 8;
+        let digit3 = (op & 0x00F0) >> 4;
+        let digit4 = op & 0x000F;
+
+        match (digit1, digit2, digit3, digit4) {
+            (0, 0, 0, 0) => {return;}, //Do nothing
+            (0, 0, 0xE, 0) => {self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];}, //Reset screen
+            (0, 0, 0xE, 0xE) => {self.pc = self.pop()}, //Return from a subroutine
+            (0x1, _, _, _) => {self.pc = op & 0x0FFF},
+            (0x2, _, _, _) => { //Enter a subroutine
+                self.push(self.pc);
+                self.pc = op & 0x0FFF;
+            },
+            (0x3, _, _, _) => { //Skip if VX == NN
+                let x = digit2 as usize;
+                let nn = (op & 0x00FF) as u8;
+                if self.v_reg[x] == nn {
+                    self.pc += 2; //Move forward 2 bytes, which is length of one instruction
+                }
+            },
+            (0x4, _, _, _) => { //Skip if VX != NN
+                let x = digit2 as usize;
+                let nn = (op & 0x00FF) as u8;
+                if self.v_reg[x] != nn {
+                    self.pc += 2;
+                }
+            },
+            (0x5, _, _, 0) => { //Skip if VX == VY
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                if (self.v_reg[x] == self.v_reg[y]) {
+                    self.pc += 2;
+                }
+            },
+            (0x6, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0x00FF) as u8;
+                self.v_reg[x] = nn;
+            },
+            (0x7, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0x00FF) as u8;
+                self.v_reg[x] = self.v_reg[x].wrapping_add(nn);
+            }
+            (0x8, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] = self.v_reg[y]
+            },
+            (0x8, _, _, 0x1) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] |= self.v_reg[y]
+            },
+            (0x8, _, _, 0x2) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] &= self.v_reg[y]
+            },
+            (0x8, _, _, 0x3) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] ^= self.v_reg[y]
+            },
+            (0x8, _, _, 0x4) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+
+                let (new_vx, overflowbool) = self.v_reg[x].overflowing_add(self.v_reg[y]);
+
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = if overflowbool { 1 } else { 0 };
+            },
+            (0x8, _, _, 0x5) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+
+                let (new_vx, underflowbool) = self.v_reg[x].overflowing_sub(self.v_reg[y]);
+
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = if underflowbool { 0 } else { 1 };
+            },
+            (0x8, _, _, 0x6) => {
+                let x = digit2 as usize;
+                let z = self.v_reg[x] & 0x1;
+
+                self.v_reg[x] >>= 1;
+                self.v_reg[0xF] = z;
+
+            },
+            (0x8, _, _, 0x7) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+
+                let (new_vx, underflowbool) = self.v_reg[y].overflowing_sub(self.v_reg[x]);
+
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = if underflowbool { 0 } else { 1 };
+            },
+            (0x8, _, _, 0xE) => {
+                let x = digit2 as usize;
+                let z = (self.v_reg[x] >> 7) & 1;
+                self.v_reg[x] <<= 1;
+                self.v_reg[0xF] = z;
+            },
+            (0x9, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                if (self.v_reg[x] != self.v_reg[y]) {
+                    self.pc += 2;
+                }
+            },
+            (0xA, _, _, _) => {
+                let nnn = (op & 0x0FFF) as u16;
+                self.i_reg = nnn
+            }
+            (0xB, _, _, _) => {
+                let nnn = (op & 0x0FFF) as u16;
+                self.pc = (self.v_reg[0] as u16) + nnn;
+            }
+            (0xC, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0x00FF) as u8;
+                self.v_reg[x] = random() & nn;
+            }
+            (0xD, _, _, _) => {
+                let x_cord = self.v_reg[digit2 as usize] as u16;
+                let y_cord = self.v_reg[digit3 as usize] as u16;
+                let height = digit4;
+
+                for row in 0..height {
+                    let adress = self.i_reg + row as u16;
+
+                }
+
+
+            }
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
+        }
+    }
+
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
+        }
+        if self.st > 0 {
+            if (self.st == 1){
+                //TODO Sound
+            }
+            self.st -= 1;
+        }
+    }
+}
+
+
---
+++ desktop/target/debug/build/getrandom-7b54a4d478cc8bb4/build_script_build-7b54a4d478cc8bb4.d
@@ -0,0 +1,5 @@
+C:\Users\karto\RustroverProjects\Chip8Emulator\desktop\target\debug\build\getrandom-7b54a4d478cc8bb4\build_script_build-7b54a4d478cc8bb4.exe: C:\Users\karto\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.1.16\build.rs
+
+C:\Users\karto\RustroverProjects\Chip8Emulator\desktop\target\debug\build\getrandom-7b54a4d478cc8bb4\build_script_build-7b54a4d478cc8bb4.d: C:\Users\karto\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.1.16\build.rs
+
+C:\Users\karto\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\getrandom-0.1.16\build.rs:
